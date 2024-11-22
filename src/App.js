6c59b293484b137ce965817539a091db60aa9a92
YellowFiber <dsa020175@gmail.com>
2024-11-22 15:24:24 +0900

    기본 작업

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Component } from "./core/heropy";
+
+export default class App extends Component {
+  constructor() {
+    super({
+      state: {
+        
+      },
+    });
+  }
+  render() {
+  }
+}
